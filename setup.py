d901fed8da65baa264d6d2f0c022e1a18c2308da
Maxim Petrov <maximpetrov@yahoo.com>
2015-01-04 16:24:34 +0300

    m

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ import os
 # for the specific client drivers/modules your application needs.
 packages = ['Django<=1.7',
             'static3',  # If you want serve the static files in the same server
+            'mysqlclient',
             # 'mysql-connector-python',
             # 'pymongo',
-            'psycopg2',
+            # 'psycopg2',
            ]
 
 if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
